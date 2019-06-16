8d7df8e49ebd3a6b84194b5858893a78d3fc6b61
Aashay Kumar <34245372+aashaykumar@users.noreply.github.com>
2019-06-17 00:38:36 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ class Main
     // Prints "Hello, World" to the terminal window. 
     public static void main(String args[]) 
     { 
-        System.out.println("Hello, World"); 
+        System.out.println("Hello, World by github"); 
     } 
 }   
 
